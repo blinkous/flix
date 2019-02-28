bd2cfacee674657dd2c2c45a95c3ab0d59d61ffc
blinkous <saradhani1@gmail.com>
2019-02-28 16:06:42 -0500

    Finished collection view and basic reqs for submission

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by stargaze on 2/28/19.
+//  Copyright © 2019 blinkous. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
