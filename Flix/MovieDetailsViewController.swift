0b01aafed732b4e42a62c7e47f253f9e91bb826f
blinkous <saradhani1@gmail.com>
2019-02-28 14:27:43 -0500

    Finished basic details screen

--- Flix/MovieDetailsViewController.swift
+++ Flix/MovieDetailsViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AlamofireImage
 
 class MovieDetailsViewController: UIViewController {
     @IBOutlet weak var backdropView: UIImageView!
@@ -18,8 +19,26 @@ class MovieDetailsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(movie["title"])
         // Do any additional setup after loading the view.
+        
+        titleLabel.text = movie["title"] as? String
+        titleLabel.sizeToFit()
+        
+        synopsisLabel.text = movie["overview"] as? String
+        synopsisLabel.sizeToFit()
+        
+        // Get the poster image
+        let baseUrl = "https://image.tmdb.org/t/p/w500"
+        let posterPath = movie["poster_path"] as! String
+        // URL has some form of validation to make sure that this is a URL
+        let posterUrl = URL(string: baseUrl + posterPath)
+        
+        // Using the pod library AlamofireImage allows us to use this to get an image from a URL and the pod will take care of downloading and getting it
+        posterView.af_setImage(withURL: posterUrl!)
+        
+        let backdropPath = movie["backdrop_path"] as! String
+        let backdropUrl = URL(string: baseUrl + backdropPath)
+        backdropView.af_setImage(withURL: backdropUrl!)
     }
     
 
